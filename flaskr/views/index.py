78603a3952b8b6fb985bb01e772c79e31816b5a2
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2015-11-22 17:45:54 +0100

    Changed how the front-page looks like

--- flaskr/views/index.py
+++ flaskr/views/index.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template, abort
 from flaskr.api.functions import get_current_user
 
+from flaskr.models import sess, Image, Place
+
 
 index = Blueprint('index', __name__,
                         template_folder='templates')
@@ -8,4 +10,7 @@ index = Blueprint('index', __name__,
 @index.route('/')
 def _index():
     current_user = get_current_user()
-    return render_template('index.html', current_user=current_user)
+
+    images = sess.query(Image, Place).join(Place).order_by(Image.created.desc()).limit(32).all()
+
+    return render_template('index.html', current_user=current_user, images=images)
